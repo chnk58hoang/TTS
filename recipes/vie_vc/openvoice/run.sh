d98a050e25a0984b16c8900805426b15e40921fa
chnk58hoang <19021285@vnu.edu.vn>
2025-06-28 18:39:06 +0000

    add sr args

--- recipes/vie_vc/openvoice/run.sh
+++ recipes/vie_vc/openvoice/run.sh
@@ -4,7 +4,7 @@
 dataset_path=/data/vivoice/
 meta_file_train=transcripts.txt
 formatter=vivoice
-batch_size=32
+batch_size=24
 epochs=100
 continue_path=
 output_path=/TTS/recipes/vie_vc/openvoice/models/"${formatter}"
@@ -13,6 +13,7 @@ checkpoint_path=/TTS/checkpoints/checkpoint.pth
 dict_phonemes_json=/TTS/TTS/tts/utils/text/vietnamese/dict_phoneme.json
 gpu=0
 accum=1
+sr=16000
 
 python train_openvoice.py \
     --dataset_path "${dataset_path}" \
@@ -20,6 +21,7 @@ python train_openvoice.py \
     --formatter "${formatter}" \
     --batch_size "${batch_size}" \
     --epochs "${epochs}" \
+    --sr "${sr}" \
     --restore_path "${continue_path}" \
     --output_path "${output_path}" \
     --test_dir "${test_dir}" \
